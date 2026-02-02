70afdeb5982b29e9dd131a6395843dd962267d28
Duc Xuan Viet Nguyen <cookie@cuberates-242.local>
2026-02-01 16:06:43 -0800

    1077: solved c + implementing b

---
+++ codeforces/1077/B.cpp
@@ -0,0 +1,108 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <random>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <thread>
+#include <algorithm>
+
+void solve();
+
+int main(void) { 
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0); std::cout.tie(0);
+  
+  uint32_t num_test = 0;   
+  std::cin >> num_test; 
+  for(uint32_t nt = 0; nt < num_test; nt++) {
+    solve(); 
+  }
+}
+
+void solve() { 
+  uint32_t n; 
+  std::string s; 
+  std::cin >> n >> s; 
+
+  std::vector<long long> dp(n, 0);
+  for(uint32_t i = 0; i < n; i++) {
+    if (s[i] == '1') dp[i] = 1 + (i-1 >= 0 ? dp[i-1] : 0); 
+    else { 
+      if ((i-1<0 || (i-1>=0 && s[i-1]!='1')) && (i+1>=n || (i+1)<n && s[i+1]!='1')) { 
+        dp[i] = std::max(dp[i], (i-1 >= 0 ? dp[i-1] : 0)); 
+        dp[i] = std::max(dp[i], (i-2 >= 0 ? dp[i-2]+1 : 0));
+      } else { 
+        dp[i] = (i-1 >= 0 ? dp[i-1] : 0);
+      }
+    }
+  }
+  std::cout << dp[n-1] << "\n";
+} 
