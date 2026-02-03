2505174e1b16b20d9f03312d8bd93e4f0269a003
Duc Xuan Viet Nguyen <cookie@cuberates-242.local>
2026-02-03 15:29:33 -0800

    1076D: binary search for the number of swords needed to pass level i

--- codeforces/1076/D.cpp
+++ codeforces/1076/D.cpp
@@ -78,64 +78,37 @@ void solve();
 int main() { 
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(0); std::cout.tie(0);
-  
   int num_test = 0;   
   std::cin >> num_test; 
   for(int nt = 0; nt < num_test; nt++) {  
     solve(); 
   }
 }
-
 void solve() {
   int n; 
   std::cin >> n; 
-  std::vector<int> a(n), b(n);
-  for(int i = 0; i < n; i++) { std::cin >> a[i]; }
+  std::vector<long long> a(n), b(n);
+  for(int i = 0; i < n; i++) { std::cin >> a[i]; } 
   for(int i = 0; i < n; i++) { std::cin >> b[i]; }
 
-  // std::sort(a.begin(), a.end()); 
+  std::sort(a.begin(), a.end(), std::greater<int>());
+  std::vector<long long> need(n+1, 0);
 
-  // binary search for x; 
-  long long l = 0, r = 1e9+1; 
-  while (r-l > 1) { 
-    int dl = ((2*l)+r)/3; 
-    int dr = (l+(2*r))/3;
-    int count_l = 0, count_r = 0; 
-    for(int i = 0; i < n; i++) { 
-      count_l += (a[i] >= dl);
-      count_r += (a[i] >= dr);
-    }
-    // std::cout << "count_l: " << count_l << "\n";
-    // std::cout << "count_r: " << count_r << "\n";
-    int ll; 
-    for(ll = 0; ll < n; ll++) { 
-      if(count_l - b[ll] >= 0) count_l-=b[ll];
-      else break;
-    }
-    int lr; 
-    for(lr=0; lr<n;lr++) { 
-      if(count_r - b[lr] >= 0) count_r-=b[lr];
-      else break;
-    }
-    long long sl = ll * dl;
-    long long sr = lr * dr;
-    // std::cout << "sl: " << sl << "\n";
-    // std::cout << "sr: " << sr << "\n"; 
-    if(sl < sr) { 
-      l = dl; 
-      r = dr;
-    } else { 
-      r = dr;
-    }
+  for(int i = 0; i <= n; i++) { 
+    if (i <= 0) need[i] = 0;
+    else need[i] = need[i-1] + b[i-1]; 
   }
-  int count = 0; 
+  
+  long long ans = -1; 
+  
   for(int i = 0; i < n; i++) { 
-    count += (a[i] >= r);
+    long long diff = a[i];
+    long long num_swords = i+1; 
+
+    long long passed = std::upper_bound(need.begin(), need.end(), num_swords)-need.begin()-1;
+    long long score = passed * diff;
+    ans = std::max(ans, score);
   }
-  int level; 
-  for(level = 0; level < n; level++) { 
-    if (count - b[level] >= 0) count -= b[level];
-    else break;
-  }
-  std::cout << level * r << "\n";
+
+  std::cout << ans << "\n";
 }
