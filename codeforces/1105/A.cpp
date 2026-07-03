c6bb57d05e37965990b4951b1ee63d1e0f89c4fc
Duc Xuan Viet Nguyen <dnxv2409@gmail.com>
2026-07-03 09:39:17 +0700

    solved 1105A

---
+++ codeforces/1105/A.cpp
@@ -0,0 +1,115 @@
+#include <random> 
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <random>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <thread>
+
+/**@attention: Random generator stolen from a random person */
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+long long rand(long long L, long long R){
+  return std::uniform_int_distribution<long long>(L, R)(rng);
+}
+
+void solve(); 
+int main() { 
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0); std::cout.tie(0);
+  
+  int num_test = 0;   
+  std::cin >> num_test; 
+  for(int nt = 0; nt < num_test; nt++) {  
+    solve(); 
+  }
+}
+
+void solve() {
+  int n; 
+  std::cin >> n;
+  std::vector<int64_t> v(n); 
+  for(int i = 0; i < n; i++) { 
+    std::cin >> v[i];
+  }
+  int64_t XOR = 0; 
+  for(int i = 0; i < n; i++) { 
+    XOR ^= v[i];
+  }
+  if (n <= 1) { std::cout << "0\n"; return; }
+  if (XOR == 0) {std::cout << "1\n"; return; }
+
+  int64_t cnt = 0; 
+  for(int i = 0; i < n; i++) { 
+    cnt += ((v[i] ^ XOR) <= v[i]); 
+  }
+  std::cout << cnt << '\n';
+  return;
+}
